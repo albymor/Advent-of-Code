7ed08ef7f2cba1e970fb62223ca811e7d046391a
Alberto Morato <alberto.morato90@gmail.com>
2022-12-06 09:48:22 +0100

    Polish out Day06

--- Day06/main.py
+++ Day06/main.py
@@ -1,49 +1,28 @@
 with open('input.txt', 'r') as f:
     data = f.read()
 
-
-
-def get_part_one(signal):
-    counter = 4
-    while True:
-        mark = set(signal[0:4])
-        if len(mark) == 4:
-            break
-        else:
-            signal = signal[1:]
-            counter +=1
     
-    return counter
+def get_result(signal, window_length):
 
-
-
-
-
-assert get_part_one('mjqjpqmgbljsphdztnvjfqwrcgsmlb') == 7
-assert get_part_one("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5
-assert get_part_one('nppdvjthqldpwncqszvftbrmjlhg') == 6
-assert get_part_one('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg') == 10
-assert get_part_one('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw') == 11
-print(f'Part 1: {get_part_one(data)}')
-
-# # part 2
-
-def get_part_two(signal):
-    counter = 14
-    while True:
-        mark = set(signal[0:14])
-        if len(mark) == 14:
+    for i in range(len(signal)-window_length):
+        if len(set(signal[i:i+window_length])) == window_length:
             break
-        else:
-            signal = signal[1:]
-            counter +=1
-    
-    return counter
+    return i+window_length
 
 
-assert get_part_two('mjqjpqmgbljsphdztnvjfqwrcgsmlb') == 19
-assert get_part_two("bvwbjplbgvbhsrlpgdmjqwftvncz") == 23
-assert get_part_two('nppdvjthqldpwncqszvftbrmjlhg') == 23
-assert get_part_two('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg') == 29
-assert get_part_two('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw') == 26
-print(f'Part 2: {get_part_two(data)}')
+assert get_result('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 4) == 7
+assert get_result("bvwbjplbgvbhsrlpgdmjqwftvncz", 4) == 5
+assert get_result('nppdvjthqldpwncqszvftbrmjlhg', 4) == 6
+assert get_result('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 4) == 10
+assert get_result('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 4) == 11
+print(f'Part 1: {get_result(data, 4)}')
+
+# part 2
+
+
+assert get_result('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 14) == 19
+assert get_result("bvwbjplbgvbhsrlpgdmjqwftvncz", 14) == 23
+assert get_result('nppdvjthqldpwncqszvftbrmjlhg', 14) == 23
+assert get_result('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 14) == 29
+assert get_result('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 14) == 26
+print(f'Part 2: {get_result(data,14)}')
