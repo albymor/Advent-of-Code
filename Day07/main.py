4005c5acf07d98c1d8c248ebfe74f6326d6dd949
Alberto Morato <alberto.morato90@gmail.com>
2022-12-07 08:50:25 +0100

    Clean Day07

--- Day07/main.py
+++ Day07/main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import re
 from copy import copy
 with open('input.txt', 'r') as f:
@@ -27,60 +28,6 @@ $ ls
 5626152 d.ext
 7214296 k"""
 
-    
-def get_part_oneb(data):
-    tree = ['root']
-    file_sizes = []
-    resume = {}
-    lfs = []
-    data = data.split('\n')
-    for l in data:
-        if "$ cd" in l:
-            path = '-'.join(tree)
-            if len(lfs) > 0:
-                file_sizes.append(lfs)
-                resume[path] = copy(file_sizes)
-                lfs = []
-            l = l.split(' ')
-            directory = l[-1]
-            if ('/' in directory):
-                tree = ['root']
-                if len(file_sizes) > 0:
-                    file_sizes = file_sizes[0]
-            elif ('..' in directory):                
-                #if len(tree) == 1:
-                file_sizes = [] #file_sizes[:-1]
-                tree = tree[:-1]
-            else:
-                tree.append(directory)
-        elif "$ ls" in l:
-            pass
-        else:
-            aa = re.findall(r'(\d+) ', l)
-            if len(aa)>0:
-                lfs.append(int(aa[0]))
-
-    path = '-'.join(tree)
-    if len(lfs) > 0:
-        file_sizes.append(lfs)
-        resume[path] = copy(file_sizes)
-
-    print(resume)
-
-
-    sum_resume = {}
-    for k in resume:
-        sum_resume[k] = sum(resume[k])
-
-    total = 0
-    for k in sum_resume:
-        if sum_resume[k] < 100000:
-            total += sum_resume[k]
-
-    print(total)
-    print(sum_resume)
-
-    return total
 
 class Node():
     def __init__(self, name, parent):
@@ -107,14 +54,10 @@ class Node():
     def has_child(self):
         return len(self.childs) > 0
 
-    
-        
-
-
 
 def get_part_one(data):
     root = Node('root', None)
-    current_node =  root
+    current_node = root
     file_sizes = []
     resume = {}
     lfs = []
@@ -125,7 +68,7 @@ def get_part_one(data):
             directory = l[-1]
             if ('/' in directory):
                 current_node = root
-            elif ('..' in directory):                
+            elif ('..' in directory):
                 current_node = current_node.get_parent()
             else:
                 n = Node(directory, current_node)
@@ -136,13 +79,13 @@ def get_part_one(data):
             pass
         else:
             aa = re.findall(r'(\d+) ', l)
-            if len(aa)>0:
+            if len(aa) > 0:
                 current_node.sizes.append(int(aa[0]))
-
 
     tt = get_size(root, 0)
 
     return tt
+
 
 def get_size(node, total):
     size = node.get_size()
@@ -154,16 +97,15 @@ def get_size(node, total):
     return total
 
 
-    
 assert get_part_one(test) == 95437
 print(f'Part 1: {get_part_one(data)}')
 
 # part 2
 
-import numpy as np
+
 def get_part_two(data):
     root = Node('root', None)
-    current_node =  root
+    current_node = root
     file_sizes = []
     resume = {}
     lfs = []
@@ -174,7 +116,7 @@ def get_part_two(data):
             directory = l[-1]
             if ('/' in directory):
                 current_node = root
-            elif ('..' in directory):                
+            elif ('..' in directory):
                 current_node = current_node.get_parent()
             else:
                 n = Node(directory, current_node)
@@ -185,28 +127,28 @@ def get_part_two(data):
             pass
         else:
             aa = re.findall(r'(\d+) ', l)
-            if len(aa)>0:
+            if len(aa) > 0:
                 current_node.sizes.append(int(aa[0]))
-
 
     tt = get_min(root, [])
 
     free = 70000000 - tt[0]
 
     aa = free + np.array(tt)
-    aa = aa[aa>30000000]
+    aa = aa[aa > 30000000]
     idx = np.argmin(aa)
 
-
     return aa[idx]-free
+
 
 def get_min(node, total):
     size = node.get_size()
     total.append(size)
     for c in node.childs:
-        total+ get_min(c, total)
+        total + get_min(c, total)
 
     return total
+
 
 assert get_part_two(test) == 24933642
 print(f'Part 1: {get_part_two(data)}')
