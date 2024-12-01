be8b7053798579cc2414d37c40c13df55ca58def
Alberto Morato <alberto.morato90@gmail.com>
2024-12-01 08:06:07 +0100

    move template

---
+++ Template/main.py
@@ -0,0 +1,24 @@
+with open('input.txt', 'r') as f:
+    data = f.read()
+
+test = """"""   
+
+def get_part_one(data):
+    lines = data.split('\n')
+
+    return 0
+
+assert get_part_one(test) == 0
+print(f'Part 1: {get_part_one(data)}')
+
+
+
+# part 2 
+def get_part_two(data):
+    lines = data.split('\n')
+
+    return 1
+
+assert get_part_two(test) == 1
+print(f'Part 2: {get_part_two(data)}')
+
