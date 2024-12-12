ac207e3ecda0220dbd6c6139e9b69aa3ad4c59af
Alberto Morato <alberto.morato90@gmail.com>
2024-12-12 18:20:33 +0100

    Day11 2024

--- 2024/Day11/main.py
+++ 2024/Day11/main.py
@@ -1,8 +1,12 @@
+from aoc.utils import timeit
+
 with open('input.txt', 'r') as f:
     data = f.read()
 
 test = """125 17"""   
 from tqdm import tqdm
+
+@timeit
 def get_part_one(data):
     data = list(map(int, data.split(' ')))
 
@@ -20,23 +24,45 @@ def get_part_one(data):
                 tmp.append(el*2024)
 
         data = tmp
-        print(data)
 
-    print(len(data))
+    return len(data)
 
-    return 0
+assert get_part_one(test) == 55312
+print(f'Part 1: {get_part_one(data)}')
 
-assert get_part_one(test) == 0
-#print(f'Part 1: {get_part_one(data)}')
+from functools import cache
+
+@cache
+def splitter(stones, step):
+    res = 0
+    if step == -1:
+        return 1
+    
+    for stone in stones:
+        tmp=[]
+         
+        if stone == 0:
+            tmp.append(1)
+        elif len(str(stone))%2 == 0:
+            a = int(str(stone)[:len(str(stone))//2])
+            b = int(str(stone)[len(str(stone))//2:])
+            tmp.append(a)
+            tmp.append(b)
+        else:
+            tmp.append(stone*2024)
+
+        res += splitter(tuple(tmp), step-1)
+
+    return res
 
 
 
 # part 2 
+@timeit
 def get_part_two(data):
-    lines = data.split('\n')
+    data = tuple(map(int, data.split(' ')))
 
-    return 1
+    return splitter(data, 75)
 
-assert get_part_two(test) == 1
 print(f'Part 2: {get_part_two(data)}')
 
